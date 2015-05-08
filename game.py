257e58b4cae8815f6efd126bbfa7f5cc8e1c0361
rachel <rachel@asana.com>
2015-05-07 23:14:48 -0700

    pre-board objects

--- game.py
+++ game.py
@@ -3,66 +3,120 @@ import random
 
 BOARD_SIZE = 4
 
-class Board():
+class Game():
     def __init__(self):
-        self.allBoards = []
-        self.board = [[0 for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
+        board = [[0 for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
         for i in range(3):
-            self.addRandomTwo()
+            self.addRandomTwo(board)
+        self.allBoards = [board]
 
-    def countZeros(self):
-        return sum([row.count(0) for row in self.board])
+    def countZeros(self, board):
+        return sum([row.count(0) for row in board])
 
-    def addRandomTwo(self):
-        add_at_zero = random.randint(1, self.countZeros())
+    def currentScore(self):
+        return sum([sum(row) for row in self.allBoards[-1]])
+
+    def addRandomTwo(self, board):
+        add_at_zero = 1 if self.countZeros(board) == 1 else random.randint(1, self.countZeros(board))
         for i in range(BOARD_SIZE):
             for j in range(BOARD_SIZE):
-                if self.board[i][j] == 0:
+                if board[i][j] == 0:
                     add_at_zero -= 1
                     if add_at_zero == 0:
-                        self.board[i][j] = 2
+                        board[i][j] = 2
                         return
 
 
-    def moveSquareUp(self, i, j, collapsed_pos):
-        current_value = self.board[i][j]
+    def moveSquareUp(self, board, i, j, collapsed_pos):
+        current_value = board[i][j]
         if current_value != 0:
             valid_pos = None
             for trial_pos in reversed(range(0, i)):
                 if trial_pos >= collapsed_pos:
-                    if self.board[trial_pos][j] == 0:
+                    if board[trial_pos][j] == 0:
                         valid_pos = trial_pos
                     else:
-                        if self.board[trial_pos][j] == current_value:
-                            self.board[trial_pos][j] += self.board[i][j]
-                            self.board[i][j] = 0
+                        if board[trial_pos][j] == current_value:
+                            board[trial_pos][j] += board[i][j]
+                            board[i][j] = 0
                             return trial_pos
                         else:
                             break
             if valid_pos is not None:
-                self.board[valid_pos][j] = self.board[i][j]
-                self.board[i][j] = 0
+                board[valid_pos][j] = board[i][j]
+                board[i][j] = 0
         return collapsed_pos
 
-    def moveUp(self):
-        print "Moving UP!!"
+    def moveUp(self, board):
+        # working through all the columns, work downwards, and push values up to their final destination
+        old_board = [row[:] for row in board]
         for j in range(BOARD_SIZE):
-            collapsed_pos = 0
+            collapsed_pos = -1
             for i in range(1, BOARD_SIZE):
-                # go left from i, until you hit the wall, or you find someone to squish into
-                collapsed_pos = self.moveSquareUp(i, j, collapsed_pos)
+                collapsed_pos = self.moveSquareUp(board, i, j, collapsed_pos)
+        return board != old_board
 
+    def rotateClockwise(self, board):
+        new_board = [[board[i][3 - j] for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
+        for i in range(BOARD_SIZE):
+            for j in range(BOARD_SIZE):
+                board[i][j] = new_board[i][j]
 
-
-    def printBoard(self):
-        for row in self.board:
+    def printBoard(self, board):
+        for row in board:
             print row
         print "\n\n"
 
+    def printCurrentBoard(self):
+        self.printBoard(self.allBoards[-1])
+
+    def makeMove(self, board, direction):
+        # rotate direction times
+        for i in range(direction):
+            self.rotateClockwise(board)
+
+        # move up
+        move_worked = self.moveUp(board)
+
+        # rotate (4 - direction) % 4 times
+        for i in range((BOARD_SIZE - direction) % 4):
+            self.rotateClockwise(board)
+
+        return move_worked
+
+    def moveIsValid(self, direction):
+        board = [row[:] for row in self.allBoards[-1]]
+        return self.makeMove(board, direction)
+
+    def takeTurn(self, direction):
+        if not self.moveIsValid(direction):
+            return False
+
+        board = [row[:] for row in self.allBoards[-1]]
+        self.addRandomTwo(board)
+        self.makeMove(board, direction)
+        self.allBoards.append(board)
+
+        print "I moved direction", direction
+        my_game.printCurrentBoard()
+        return True
+
+
+class Player1():
+    def move(self, board):
+        return
+
 
 if __name__ == "__main__":
-    board = Board()
-    board.printBoard()
+    my_game = Game()
 
-    board.moveUp()
-    board.printBoard()
+    while 1:
+        c = raw_input('helllo')
+        print  c, "xcxc", c == u"\u2192"
+
+    for i in range(400):
+        valid_move = my_game.takeTurn((i % 4))
+        if not valid_move:
+            print "YOU SUCK, SCORE ", my_game.currentScore()
+            break
+
