0f3bcbab06ee6caa6ffe263041baa33c576ce701
rachel <rachel@asana.com>
2015-05-10 13:12:35 -0700

    working

--- game.py
+++ game.py
@@ -85,61 +85,12 @@ class Game():
     def current_score(self):
         return self.allBoards[-1].score()
 
-    def move_square_up(self, board, i, j, collapsed_pos):
-        current_value = board[i][j]
-        if current_value != 0:
-            valid_pos = None
-            for trial_pos in reversed(range(0, i)):
-                if trial_pos >= collapsed_pos:
-                    if board[trial_pos][j] == 0:
-                        valid_pos = trial_pos
-                    else:
-                        if board[trial_pos][j] == current_value:
-                            board[trial_pos][j] += board[i][j]
-                            board[i][j] = 0
-                            return trial_pos
-                        else:
-                            break
-            if valid_pos is not None:
-                board[valid_pos][j] = board[i][j]
-                board[i][j] = 0
-        return collapsed_pos
-
-    def move_up(self, board):
-        # working through all the columns, work downwards, and push values up to their final destination
-        old_board = [row[:] for row in board]
-        for j in range(BOARD_SIZE):
-            collapsed_pos = -1
-            for i in range(1, BOARD_SIZE):
-                collapsed_pos = self.move_square_up(board, i, j, collapsed_pos)
-        return board != old_board
-
-    def rotate_clockwise(self, board):
-        new_board = [[board[i][3 - j] for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
-        for i in range(BOARD_SIZE):
-            for j in range(BOARD_SIZE):
-                board[i][j] = new_board[i][j]
-
     def print_current_board(self):
         self.allBoards[-1].print_board()
 
-    def make_move(self, board, direction):
-        # rotate direction times
-        for i in range(direction):
-            self.rotate_clockwise(board)
-
-        # move up
-        move_worked = self.move_up(board)
-
-        # rotate (4 - direction) % 4 times
-        for i in range((BOARD_SIZE - direction) % 4):
-            self.rotate_clockwise(board)
-
-        return move_worked
-
     def is_move_valid(self, direction):
-        board = Board(self.allBoards[-1])
-        return self.make_move(board.board, direction)
+        current_board_copy = Board(self.allBoards[-1])
+        return current_board_copy.make_move(direction)
 
     def take_turn(self, direction):
         if not self.is_move_valid(direction):
@@ -147,25 +98,31 @@ class Game():
 
         board = Board(self.allBoards[-1])
         board.add_random_two()
-        self.make_move(board.board, direction)
+        board.make_move(direction)
         self.allBoards.append(board)
 
         print "I moved direction", direction
         my_game.print_current_board()
         return True
 
+    def current_board(self):
+        return self.allBoards[-1]
+
 
 class Player1():
-    def move(self, board):
-        return
+    def move(self, game):
+        current_score = game.current_board().score()
+        return (current_score / 2) % 4
 
 
 if __name__ == "__main__":
     my_game = Game()
+    player = Player1()
 
-    for main_i in range(400):
-        valid_move = my_game.take_turn((main_i % 4))
+    while True:
+        valid_move = my_game.take_turn(player.move(my_game))
         if not valid_move:
-            print "YOU SUCK, SCORE ", my_game.current_score()
+            print "Invalid move - Game over!"
+            print "Score: ", my_game.current_score()
             break
 
